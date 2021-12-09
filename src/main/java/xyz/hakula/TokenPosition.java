abd63a3f74ce1d40845baab32bde5aeeb7450405
Hakula Chen <i@hakula.xyz>
2021-12-09 18:02:12 +0800

    fix: wrong position issue (wip)

--- src/main/java/xyz/hakula/TokenPosition.java
+++ src/main/java/xyz/hakula/TokenPosition.java
@@ -11,45 +11,41 @@ import java.util.Locale;
 import java.util.StringTokenizer;
 
 public class TokenPosition {
+  private static final String DELIM = ":";
   private static final String TOKEN_DELIM = "@";
-  private static final String POS_DELIM = ",";
   private static final String POS_LIST_DELIM = ";";
 
-  public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+  public static class Map extends Mapper<LongWritable, Text, Text, LongWritable> {
     private final Text key = new Text();
-    private final Text position = new Text();
+    private final LongWritable position = new LongWritable();
 
     // Yield a position of a token in each file.
     @Override
     public void map(LongWritable key, Text value, Context context)
         throws IOException, InterruptedException {
       var filename = ((FileSplit) context.getInputSplit()).getPath().getName();
-      var row = key.get() + 1;
-      var col = 1;
+      var position = key.get() + 1;
 
       var it = new StringTokenizer(value.toString(), " \t\r\f");
       while (it.hasMoreTokens()) {
         var token = it.nextToken().toLowerCase(Locale.ROOT);
-        // Suppose all words are separated with a single whitespace character.
-        col += token.length() + 1;
-
         this.key.set(token + TOKEN_DELIM + filename);
-        position.set(row + POS_DELIM + col);
-        context.write(this.key, position);
+        this.position.set(position);
+        context.write(this.key, this.position);
+
+        // Suppose all words are separated with a single whitespace character.
+        position += token.length() + 1;
       }
     }
   }
 
-  public static class Reduce extends Reducer<Text, Text, Text, Text> {
+  public static class Reduce extends Reducer<Text, LongWritable, Text, Text> {
     private final Text value = new Text();
 
     // Yield all positions of a token in each file and its number of occurrence.
     @Override
-    public void reduce(Text key, Iterable<Text> values, Context context)
+    public void reduce(Text key, Iterable<LongWritable> values, Context context)
         throws IOException, InterruptedException {
-      var items = key.toString().split(TOKEN_DELIM);
-      var token = items[0];
-
       var positions = new StringBuilder();
       long tokenCount = 0;
       for (var value : values) {
