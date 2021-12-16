7a4d417bf433b5ddcdef220660fba043f2dd2d1c
hakula139 <1517242376@qq.com>
2021-12-16 11:28:54 +0800

    refactor: modify InverseDocumentFreq to Java 8 style

--- src/main/java/xyz/hakula/woogle/model/InverseDocumentFreq.java
+++ src/main/java/xyz/hakula/woogle/model/InverseDocumentFreq.java
@@ -2,13 +2,29 @@ package xyz.hakula.woogle.model;
 
 import java.util.regex.Pattern;
 
-public record InverseDocumentFreq(long fileCount, double inverseDocumentFreq) {
+public class InverseDocumentFreq {
   private static final String DELIM = " ";
 
+  private final long fileCount;
+  private final double inverseDocumentFreq;
+
+  public InverseDocumentFreq(long fileCount, double inverseDocumentFreq) {
+    this.fileCount = fileCount;
+    this.inverseDocumentFreq = inverseDocumentFreq;
+  }
+
   public static InverseDocumentFreq parse(String entry) {
-    var entrySplit = entry.split(Pattern.quote(DELIM));
-    var fileCount = Long.parseLong(entrySplit[0]);
-    var inverseDocumentFreq = Double.parseDouble(entrySplit[1]);
+    String[] entrySplit = entry.split(Pattern.quote(DELIM));
+    long fileCount = Long.parseLong(entrySplit[0]);
+    double inverseDocumentFreq = Double.parseDouble(entrySplit[1]);
     return new InverseDocumentFreq(fileCount, inverseDocumentFreq);
+  }
+
+  public long fileCount() {
+    return fileCount;
+  }
+
+  public double inverseDocumentFreq() {
+    return inverseDocumentFreq;
   }
 }
